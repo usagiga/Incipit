0bdce958265ce80acf4ef906f578384726c511c3
usagiga <usagigawara@gmail.com>
2020-06-02 14:35:43 +0900

    Wrote draft of main.go

--- back/main.go
+++ back/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import "log"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Load config
+	// TODO : Load from JSON
+	mysqlUser := "incipit"
+	mysqlPass := "incipit-password"
+	mysqlHost := "db"
+	mysqlPort := 3304
+
+	// Connect to DB
+	db := ConnectToDB(mysqlUser, mysqlPass, mysqlHost, mysqlPort)
+	defer db.Close()
+
+	// Auto migrate
+	err := Migrate(db)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Build modules
+
+
+	// Register to gin
+
+
+	// Launch
+	
 }
