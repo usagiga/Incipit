e2f779bcde8b4bba07a173ce43bb5c9a7a6884fb
usagiga <usagigawara@gmail.com>
2020-06-02 20:00:14 +0900

    Implement AdminModel

--- back/model/admin.go
+++ back/model/admin.go
@@ -1,33 +1,98 @@
 package model
 
 import (
+	"errors"
+	"github.com/jinzhu/gorm"
 	"github.com/usagiga/Incipit/back/entity"
 )
 
 type AdminModelImpl struct {
-
+	db *gorm.DB
 }
 
-func NewAdminModel() AdminModel {
-	return &AdminModelImpl{}
+func NewAdminModel(db *gorm.DB) AdminModel {
+	return &AdminModelImpl{
+		db: db,
+	}
 }
 
 func (m *AdminModelImpl) Add(user *entity.AdminUser) (added *entity.AdminUser, err error) {
-	panic("implement me")
+	// Add the row
+	result := m.db.Create(user)
+	err = result.Error
+	if err != nil {
+		return nil, err
+	}
+	if result.RowsAffected <= 0 {
+		return nil, errors.New("AdminModel.Add(): Can't add it")
+	}
+
+	return user, err
 }
 
 func (m *AdminModelImpl) FindOne(id uint) (user *entity.AdminUser, err error) {
-	panic("implement me")
+	user = &entity.AdminUser{}
+
+	// Find the row
+	result := m.db.First(user, id)
+	err = result.Error
+	if result.RecordNotFound() {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return user, err
 }
 
 func (m *AdminModelImpl) Find() (users []entity.AdminUser, err error) {
-	panic("implement me")
+	// Find all rows
+	result := m.db.Find(&users)
+	err = result.Error
+	if result.RecordNotFound() {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return users, err
 }
 
 func (m *AdminModelImpl) Update(updating *entity.AdminUser) (updated *entity.AdminUser, err error) {
-	panic("implement me")
+	// Finding the row
+	found, err := m.FindOne(updating.ID)
+	if err != nil {
+		return nil, err
+	}
+	if found == nil {
+		return nil, errors.New("AdminModel.Update(): Not found updating row")
+	}
+
+	// Update the row
+	result := m.db.Model(&entity.AdminUser{}).Update(updating)
+	err = result.Error
+	if err != nil {
+		return nil, err
+	}
+	if result.RowsAffected <= 0 {
+		return updating, nil
+	}
+
+	return updating, err
 }
 
 func (m *AdminModelImpl) Delete(id uint) (err error) {
-	panic("implement me")
+	// Delete the row
+	result := m.db.Unscoped().Delete(&entity.AdminUser{}, id)
+	err = result.Error
+	if err != nil {
+		return nil
+	}
+	if result.RowsAffected <= 0 {
+		return errors.New("AdminModel.Delete(): Can't delete it")
+	}
+
+	return nil
 }
