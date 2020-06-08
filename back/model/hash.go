3560fb7aa51fb3d9582cc179e3f161bf0a38655b
usagiga <usagigawara@gmail.com>
2020-06-08 19:44:01 +0900

    Revise error in HashModel

--- back/model/hash.go
+++ back/model/hash.go
@@ -1,8 +1,11 @@
 package model
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	interr "github.com/usagiga/Incipit/back/entity/errors"
+	"golang.org/x/crypto/bcrypt"
+)
 
-type HashModelImpl struct {}
+type HashModelImpl struct{}
 
 func NewHashModel() HashModel {
 	return &HashModelImpl{}
@@ -13,7 +16,7 @@ func (h *HashModelImpl) Generate(pass string) (hashed string, err error) {
 
 	hashedBytes, err := bcrypt.GenerateFromPassword(passBytes, bcrypt.DefaultCost)
 	if err != nil {
-		return "", err
+		return "", interr.NewDistinctError("Can't generate hash", interr.HashModel, interr.HashModel_FailedGenerate, nil).Wrap(err)
 	}
 
 	hashed = string(hashedBytes)
@@ -25,6 +28,10 @@ func (h *HashModelImpl) Equals(hashedPassword, rawPassword string) (err error) {
 	hashedBytes := []byte(hashedPassword)
 	rawBytes := []byte(rawPassword)
 
-	return bcrypt.CompareHashAndPassword(hashedBytes, rawBytes)
-}
+	err = bcrypt.CompareHashAndPassword(hashedBytes, rawBytes)
+	if err != nil {
+		return interr.NewDistinctError("Don't match hash and password", interr.HashModel, interr.HashModel_FailedCompare, nil).Wrap(err)
+	}
 
+	return nil
+}
