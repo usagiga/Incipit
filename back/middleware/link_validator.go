cfbb73c5adc4f912dd765711783e73a6bd4b02bc
usagiga <usagigawara@gmail.com>
2020-06-08 23:51:43 +0900

    Implement validation rule to LinkValidator

--- back/middleware/link_validator.go
+++ back/middleware/link_validator.go
@@ -3,10 +3,13 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/usagiga/Incipit/back/entity"
+	interr "github.com/usagiga/Incipit/back/entity/errors"
+	"golang.org/x/xerrors"
+	urllib "net/url"
 )
 
 type LinkValidatorImpl struct {
-
+	incipitHost string
 }
 
 func NewLinkValidator() LinkValidator {
@@ -18,7 +21,21 @@ func (m *LinkValidatorImpl) Handle(c *gin.Context) {
 }
 
 func (m *LinkValidatorImpl) ValidateAll(link *entity.Link) (err error) {
-	panic("implement me")
+	errChan := make(chan error, 2)
+
+	go func() {
+		errChan <- m.ValidateID(link.ID)
+		errChan <- m.ValidateURL(link.URL)
+		close(errChan)
+	}()
+
+	for err := range errChan {
+		if err != nil {
+			return xerrors.Errorf("This link is invalid: %w", err)
+		}
+	}
+
+	return nil
 }
 
 func (m *LinkValidatorImpl) ValidateID(id uint) (err error) {
@@ -26,5 +43,25 @@ func (m *LinkValidatorImpl) ValidateID(id uint) (err error) {
 }
 
 func (m *LinkValidatorImpl) ValidateURL(url string) (err error) {
-	panic("implement me")
+	errChan := make(chan error, 2)
+	incipitHost := m.incipitHost
+
+	go func() {
+		parsed, err := urllib.ParseRequestURI(url)
+		if err != nil {
+			errChan <- interr.NewDistinctError("Invalid URL", interr.LinkValidation, interr.LinkValidation_URLIsInvalid, nil).Wrap(err)
+		}
+		if parsed.Host == incipitHost {
+			errChan <- interr.NewDistinctError("This URL is itself", interr.LinkValidation, interr.LinkValidation_URLIsIncipit, nil)
+		}
+		close(errChan)
+	}()
+
+	for err := range errChan {
+		if err != nil {
+			return xerrors.Errorf("This URL is invalid: %w", err)
+		}
+	}
+
+	return nil
 }
