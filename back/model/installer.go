7ad1f01ddabc7420a21974366fb5771f36403fda
usagiga <usagigawara@gmail.com>
2020-06-19 23:56:20 +0900

    Implement IsNeededInstall()

--- back/model/installer.go
+++ back/model/installer.go
@@ -8,10 +8,13 @@ import (
 type InstallerModelImpl struct {
 	adminModel     AdminModel
 	adminAuthModel AdminAuthModel
+
+	// Cache for `IsNeededInstall()`
+	isNeeded bool
 }
 
 func NewInstallerModel(adminModel AdminModel, adminAuthModel AdminAuthModel) InstallerModel {
-	return &InstallerModelImpl{adminModel: adminModel, adminAuthModel: adminAuthModel}
+	return &InstallerModelImpl{adminModel: adminModel, adminAuthModel: adminAuthModel, isNeeded: true}
 }
 
 func (m *InstallerModelImpl) CreateNewAdmin(name, screenName, password string) (accToken *entity.AccessToken, refToken *entity.RefreshToken, err error) {
@@ -37,5 +40,24 @@ func (m *InstallerModelImpl) CreateNewAdmin(name, screenName, password string) (
 }
 
 func (m *InstallerModelImpl) IsNeededInstall() (isNeeded bool, err error) {
-	panic("implement me")
+	// Use cached result
+	// Once isNeeded turn false, Incipit return cache value
+	if !m.isNeeded {
+		return false, nil
+	}
+
+	// Find all admin
+	// TODO : `Find()` should be `First()` with no condition or `Count()`
+	users, err := m.adminModel.Find()
+	if err != nil {
+		return false, xerrors.Errorf("Can't find admin users: %w", err)
+	}
+
+	// Judge
+	isNeeded = len(users) <= 0
+
+	// Store result into cache
+	m.isNeeded = isNeeded
+
+	return isNeeded, nil
 }
