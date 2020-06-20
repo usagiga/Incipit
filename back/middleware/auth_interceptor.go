5c403da16a14f87b6f11cc2562a410eb2628cd3e
usagiga <usagigawara@gmail.com>
2020-06-21 00:38:54 +0900

    Implement AuthInterceptor

--- back/middleware/auth_interceptor.go
+++ back/middleware/auth_interceptor.go
@@ -2,17 +2,39 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/usagiga/Incipit/back/entity/messages"
+	"github.com/usagiga/Incipit/back/model"
+	"net/http"
+	"strings"
 )
 
-// AuthInterceptor represents ...
 type AuthInterceptorImpl struct {
-
+	adminAuthModel model.AdminAuthModel
 }
 
-func NewAuthInterceptor() AuthInterceptor {
-	return &AuthInterceptorImpl{}
+func NewAuthInterceptor(adminAuthModel model.AdminAuthModel) AuthInterceptor {
+	return &AuthInterceptorImpl{adminAuthModel: adminAuthModel}
 }
 
 func (i *AuthInterceptorImpl) Handle(c *gin.Context) {
-	panic("implement me")
+	authHeader := c.GetHeader("Authorization")
+	splitHeader := strings.Split(authHeader, "Bearer ")
+	if len(splitHeader) != 2 {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, "{}")
+		return
+	}
+	accToken := splitHeader[1]
+
+	// Authorize
+	user, err := i.adminAuthModel.Authorize(accToken)
+	if err != nil {
+		resp := messages.NewErrorResponse(err)
+		sCode := resp.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, resp)
+		return
+	}
+
+	// Set user into ctx
+	c.Set("user", user)
 }
