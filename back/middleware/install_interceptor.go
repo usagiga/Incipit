b5075414ef20ce69f87ab4a14a5b12d1cde6535b
usagiga <usagigawara@gmail.com>
2020-06-20 21:00:52 +0900

    Implement InstallInterceptor

--- back/middleware/install_interceptor.go
+++ back/middleware/install_interceptor.go
@@ -1,15 +1,34 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/usagiga/Incipit/back/entity/messages"
+	"github.com/usagiga/Incipit/back/model"
+)
 
 type InstallInterceptorImpl struct {
-
+	installerModel model.InstallerModel
 }
 
-func NewInstallInterceptor() InstallInterceptor {
-	return &InstallInterceptorImpl{}
+func NewInstallInterceptor(installerModel model.InstallerModel) InstallInterceptor {
+	return &InstallInterceptorImpl{installerModel: installerModel}
 }
 
 func (m *InstallInterceptorImpl) Handle(c *gin.Context) {
-	panic("implement me")
+	isNeeded, err := m.installerModel.IsNeededInstall()
+	if err != nil {
+		resp := messages.NewErrorResponse(err)
+		sCode := resp.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, resp)
+		return
+	}
+
+	if isNeeded {
+		resp := messages.NewRequiredInstallResponse()
+		sCode := resp.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, resp)
+		return
+	}
 }
