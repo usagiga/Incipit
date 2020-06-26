fe8666e240dee58181d67f70c705e006d5094672
usagiga <usagigawara@gmail.com>
2020-06-27 00:32:23 +0900

    Implement AdminUserHandler

--- back/handler/admin_user.go
+++ back/handler/admin_user.go
@@ -1,27 +1,130 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/usagiga/Incipit/back/entity"
+	"github.com/usagiga/Incipit/back/entity/messages"
+	"github.com/usagiga/Incipit/back/model"
+)
 
 type AdminUserHandlerImpl struct {
-
+	adminModel model.AdminModel
 }
 
-func NewAdminUserHandler() AdminUserHandler {
-	return 	&AdminUserHandlerImpl{}
+func NewAdminUserHandler(adminModel model.AdminModel) AdminUserHandler {
+	return &AdminUserHandlerImpl{adminModel: adminModel}
 }
 
 func (h *AdminUserHandlerImpl) HandleGetAdmin(c *gin.Context) {
-	panic("implement me")
+	req := &messages.GetAdminRequest{}
+	err := c.BindJSON(req)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	found, err := h.adminModel.FindOne(req.ID)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	res := messages.NewGetAdminResponse(found)
+	sCode := res.GetHTTPStatusCode()
+
+	c.JSON(sCode, res)
 }
 
 func (h *AdminUserHandlerImpl) HandleCreateAdmin(c *gin.Context) {
-	panic("implement me")
+	req := &messages.CreateAdminRequest{}
+	err := c.BindJSON(req)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	adding := &entity.AdminUser{
+		Name:       req.Name,
+		ScreenName: req.ScreenName,
+		Password:   req.Password,
+	}
+	added, err := h.adminModel.Add(adding)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	res := messages.NewCreateAdminResponse(added)
+	sCode := res.GetHTTPStatusCode()
+
+	c.JSON(sCode, res)
 }
 
 func (h *AdminUserHandlerImpl) HandleUpdateAdmin(c *gin.Context) {
-	panic("implement me")
+	req := &messages.UpdateAdminRequest{}
+	err := c.BindJSON(req)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	updating := &entity.AdminUser{
+		Name:       req.Name,
+		ScreenName: req.ScreenName,
+		Password:   req.Password,
+	}
+	updated, err := h.adminModel.Update(updating)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	res := messages.NewUpdateAdminResponse(updated)
+	sCode := res.GetHTTPStatusCode()
+
+	c.JSON(sCode, res)
 }
 
 func (h *AdminUserHandlerImpl) HandleDeleteAdmin(c *gin.Context) {
-	panic("implement me")
+	req := &messages.DeleteAdminRequest{}
+	err := c.BindJSON(req)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	err = h.adminModel.Delete(req.ID)
+	if err != nil {
+		res := messages.NewErrorResponse(err)
+		sCode := res.GetHTTPStatusCode()
+
+		c.AbortWithStatusJSON(sCode, res)
+		return
+	}
+
+	res := messages.NewDeleteAdminResponse()
+	sCode := res.GetHTTPStatusCode()
+
+	c.JSON(sCode, res)
 }
