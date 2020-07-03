01615aac8b3db905772e873fc2f70f2d535adfbb
usagiga <usagigawara@gmail.com>
2020-07-04 01:52:27 +0900

    Implement ToActualID, GetShortID in Link

--- back/entity/link.go
+++ back/entity/link.go
@@ -1,6 +1,10 @@
 package entity
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/xerrors"
+	"kkn.fi/base62"
+)
 
 // Link represents URL to shorten
 type Link struct {
@@ -10,10 +14,15 @@ type Link struct {
 
 // GetShortID gets ID as base62 string from its actual ID
 func (e *Link) GetShortID() string {
-	panic("not implemented")
+	return base62.Encode(int64(e.ID))
 }
 
 // ToActualID gets actual ID from specified short ID
-func ToActualID(shortId string) uint {
-	panic("not implemented")
+func ToActualID(shortId string) (actualId uint, err error) {
+	rawId, err := base62.Decode(shortId)
+	if err != nil {
+		return 0, xerrors.Errorf("Can't parse actual id: %w", err)
+	}
+
+	return uint(rawId), nil
 }
