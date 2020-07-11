54cf05235e30eff573094079f8524bef3043dce9
usagiga <usagigawara@gmail.com>
2020-07-11 19:47:23 +0900

    Delete unused files

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -9,16 +9,15 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>
+            Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower
+            developers to create amazing applications.
+          </p>
           <p>
             For more information on Vuetify, check out the <a
               href="https://vuetifyjs.com"
@@ -45,7 +44,10 @@
               issue board
             </a>.
           </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing more exciting features in the
+            future.
+          </p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
@@ -64,29 +66,15 @@
             Nuxt GitHub
           </a>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import { Vue, Component } from 'nuxt-property-decorator'
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+@Component
+export default class Index extends Vue {
 }
 </script>
