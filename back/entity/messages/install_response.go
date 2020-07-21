68906124f4f09ab50020251011592cc0d3a25c79
usagiga <usagigawara@gmail.com>
2020-07-21 22:54:28 +0900

    Revise InstallResponse

--- back/entity/messages/install_response.go
+++ back/entity/messages/install_response.go
@@ -1,16 +1,25 @@
 package messages
 
 import (
+	"github.com/usagiga/Incipit/back/entity"
 	"net/http"
 )
 
 type InstallResponse struct {
 	BaseResponse
+
+	AccessToken  AccessToken  `json:"access_token"`
+	RefreshToken RefreshToken `json:"refresh_token"`
 }
 
-func NewInstallResponse() (resp *InstallResponse) {
+func NewInstallResponse(accToken *entity.AccessToken, refToken *entity.RefreshToken) (resp *InstallResponse) {
+	resAccToken := AccessToken{Token: accToken.Token}
+	resRefToken := RefreshToken{Token: refToken.Token}
+
 	return &InstallResponse{
 		BaseResponse: BaseResponse{Type: "install", Details: nil},
+		AccessToken: resAccToken,
+		RefreshToken: resRefToken,
 	}
 }
 
