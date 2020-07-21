afd2c3ce915560a40f25b0246999945bfab12873
usagiga <usagigawara@gmail.com>
2020-07-21 21:54:18 +0900

    Implement test for HandleRedundantInstall()

--- back/middleware/install_interceptor_test.go
+++ back/middleware/install_interceptor_test.go
@@ -67,6 +67,61 @@ func TestInstallInterceptorImpl_HandleNeededInstall(t *testing.T) {
 	}
 }
 
+func TestInstallInterceptorImpl_HandleRedundantInstall(t *testing.T) {
+	// Declare test cases
+	testCases := []struct {
+		ExpectedStatusCode    int
+		ExpectedResponseType  string
+		TestingInstallerModel model.InstallerModel
+	}{
+		{ExpectedStatusCode: http.StatusOK, ExpectedResponseType: "", TestingInstallerModel: installerModelStub{isNeeded: true, err: nil}},
+		{ExpectedStatusCode: http.StatusGone, ExpectedResponseType: "redundant_install", TestingInstallerModel: installerModelStub{isNeeded: false, err: nil}},
+		{ExpectedStatusCode: http.StatusInternalServerError, ExpectedResponseType: "error", TestingInstallerModel: installerModelStub{isNeeded: false, err: xerrors.New("something wrong")}},
+	}
+
+	// Do test
+	for i, v := range testCases {
+		caseNum := i + 1
+		w := httptest.NewRecorder()
+		ctx, _ := gin.CreateTestContext(w)
+		ii := &InstallInterceptorImpl{installerModel: v.TestingInstallerModel}
+
+		// Exec
+		ii.HandleRedundantInstall(ctx)
+
+		// Check status code
+		expectedCode := v.ExpectedStatusCode
+		actualCode := w.Code
+		if expectedCode != http.StatusOK && !ctx.IsAborted() {
+			t.Errorf("Case %d: Not aborted response", caseNum)
+		}
+		if actualCode != expectedCode {
+			t.Errorf("Case %d: Not valid HTTP status code of its response: Expected: %d, Actual: %d", caseNum, expectedCode, actualCode)
+		}
+
+		// Check response type
+		expectedRespType := v.ExpectedResponseType
+
+		actualRespBodyBytes := []byte(w.Body.String())
+		if len(actualRespBodyBytes) == 0 && expectedRespType == "" {
+			continue
+		}
+
+		res := &messages.BaseResponse{}
+		err := json.Unmarshal(actualRespBodyBytes, res)
+		if err != nil {
+			t.Errorf("Case %d: Can't unmarshal response JSON", caseNum)
+			continue
+		}
+
+		actualRespType := res.Type
+
+		if actualRespType != expectedRespType {
+			t.Errorf("Case %d: Not valid response body:\nExpected: %s\nActual: %s", caseNum, expectedRespType, actualRespType)
+		}
+	}
+}
+
 type installerModelStub struct {
 	isNeeded bool
 	err      error
