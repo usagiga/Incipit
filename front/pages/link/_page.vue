11b8d3c467ca8fda95246a125e28b1c64e7cda9a
usagiga <usagigawara@gmail.com>
2020-09-05 15:55:44 +0900

    Implement actions behavior in Links

--- front/pages/link/_page.vue
+++ front/pages/link/_page.vue
@@ -1,23 +1,52 @@
 <template>
   <div class="link-list">
     <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            ID
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-list-item-title>
+            Actual URL
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
       <v-list-item
-        v-for="item in items"
+        v-for="(item, index) in linkItems"
         :key="item.title"
+        class="link-list-item"
       >
         <v-list-item-content>
-          <v-list-item-title v-text="item.title" />
+          <v-list-item-title v-show="!item.isEditing" v-text="item.id" />
+          <v-list-item-title v-show="item.isEditing" v-text="item.id" />
+        </v-list-item-content>
+
+        <v-list-item-content>
+          <v-list-item-subtitle v-show="!item.isEditing" v-text="item.actualUrl" />
+          <v-text-field v-show="item.isEditing" v-model="item.editingUrl" placeholder="Actual URL" />
         </v-list-item-content>
 
         <v-list-item-action class="link-list-action">
-          <v-btn icon>
+          <v-btn v-show="!item.isEditing" icon @click="editLink(item)">
             <v-icon color="grey lighten-1">
               mdi-pencil
             </v-icon>
           </v-btn>
-          <v-btn icon>
+          <v-btn v-show="!item.isEditing" icon @click="deleteLink(item, index)">
             <v-icon color="grey lighten-1">
               mdi-delete
+            </v-icon>
+          </v-btn>
+          <v-btn v-show="item.isEditing" icon @click="submitEditLink(item)">
+            <v-icon color="grey lighten-1">
+              mdi-checkbox-marked-circle-outline
+            </v-icon>
+          </v-btn>
+          <v-btn v-show="item.isEditing" icon @click="cancelEditLink(item)">
+            <v-icon color="grey lighten-1">
+              mdi-close-circle-outline
             </v-icon>
           </v-btn>
         </v-list-item-action>
@@ -36,18 +65,56 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 
+class LinkItem {
+    id: string
+    actualUrl: string
+    editingUrl: string = ''
+    isEditing: boolean = false
+
+    constructor (id: string, actualUrl: string) {
+      this.id = id
+      this.actualUrl = actualUrl
+    }
+}
+
   @Component
 export default class LinkList extends Vue {
     page: number = 1
     pageParam: number = 1
     length: number = 10
 
-    items = [
-      { title: 'Test A' },
-      { title: 'Test B' },
-      { title: 'Test C' },
-      { title: 'Test D' }
+    linkItems: LinkItem[] = [
+      new LinkItem('Test A', 'https://example.com/'),
+      new LinkItem('Test B', 'https://example.com/'),
+      new LinkItem('Test C', 'https://example.com/')
     ]
+
+    deleteLink (linkItem: LinkItem, index: number) {
+      // TODO : Delete link through API
+      // eslint-disable-next-line no-console
+      console.log(`Deleted ${linkItem.id}` !)
+      this.linkItems.splice(index, 1)
+    }
+
+    editLink (linkItem: LinkItem) {
+      linkItem.editingUrl = linkItem.actualUrl
+
+      // Change actions visibility
+      linkItem.isEditing = true
+    }
+
+    submitEditLink (linkItem: LinkItem) {
+      // TODO : Update link through API
+      linkItem.actualUrl = linkItem.editingUrl
+
+      // Change actions visibility
+      linkItem.isEditing = false
+    }
+
+    cancelEditLink (linkItem: LinkItem) {
+      // Change actions visibility
+      linkItem.isEditing = false
+    }
 
     movePage () {
       if (this.pageParam === this.page) {
@@ -80,8 +147,10 @@ export default class LinkList extends Vue {
 
 <style lang="scss">
   .link-list {
-    .link-list-action {
-      flex-direction: row;
+    .link-list-item {
+      .link-list-action {
+        flex-direction: row;
+      }
     }
   }
 </style>
