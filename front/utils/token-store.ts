db16cf1a402ebac05a34d06463fe30d7a2011205
usagiga <usagigawara@gmail.com>
2020-09-05 19:18:34 +0900

    Add TokenStore as utils

---
+++ front/utils/token-store.ts
@@ -0,0 +1,50 @@
+/**
+ * TokenStore provides functions to treat access / refresh token
+ */
+class TokenStore {
+  private _accessTokenKey: string = 'incipit_access_token'
+  private _refreshTokenKey: string = 'incipit_refresh_token'
+
+  /**
+   * Get access token from local storage.
+   * If there's no token, return empty string.
+   */
+  get accessToken (): string {
+    const token = localStorage.getItem(this._accessTokenKey)
+    if (token === null) {
+      return ''
+    }
+
+    return token
+  }
+
+  /**
+   * Set access token into local storage.
+   */
+  set accessToken (token: string) {
+    localStorage.setItem(this._accessTokenKey, token)
+  }
+
+  /**
+   * Get refresh token from local storage.
+   * If there's no token, return empty string.
+   */
+  get refreshToken (): string {
+    const token = localStorage.getItem(this._refreshTokenKey)
+    if (token === null) {
+      return ''
+    }
+
+    return token
+  }
+
+  /**
+   * Set refresh token into local storage.
+   */
+  set refreshToken (token: string) {
+    localStorage.setItem(this._refreshTokenKey, token)
+  }
+}
+
+// Export
+export default new TokenStore()
