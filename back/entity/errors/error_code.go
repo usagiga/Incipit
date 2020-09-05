1e721158eed7dd968fa8dc34b61300f61266d7a2
usagiga <usagigawara@gmail.com>
2020-09-06 01:11:50 +0900

    Revise error codes

--- back/entity/errors/error_code.go
+++ back/entity/errors/error_code.go
@@ -10,21 +10,21 @@ func (c *PrimaryErrorCode) String() string {
 
 const (
 	// Validation
-	AdminUserValidation PrimaryErrorCode = 0101
-	LinkValidation      PrimaryErrorCode = 0102
+	AdminUserValidation PrimaryErrorCode = 101
+	LinkValidation      PrimaryErrorCode = 102
 
 	// Model
-	AdminModel     PrimaryErrorCode = 0201
-	AdminAuthModel PrimaryErrorCode = 0202
-	HashModel      PrimaryErrorCode = 0203
-	InstallerModel PrimaryErrorCode = 0204
-	LinkModel      PrimaryErrorCode = 0205
+	AdminModel     PrimaryErrorCode = 201
+	AdminAuthModel PrimaryErrorCode = 202
+	HashModel      PrimaryErrorCode = 203
+	InstallerModel PrimaryErrorCode = 204
+	LinkModel      PrimaryErrorCode = 205
 
 	// Handler
-	AdminUserHandler PrimaryErrorCode = 0301
-	AdminAuthHandler PrimaryErrorCode = 0302
-	LinkHandler      PrimaryErrorCode = 0303
-	InstallerHandler      PrimaryErrorCode = 0304
+	AdminUserHandler PrimaryErrorCode = 301
+	AdminAuthHandler PrimaryErrorCode = 302
+	LinkHandler      PrimaryErrorCode = 303
+	InstallerHandler      PrimaryErrorCode = 304
 )
 
 type SecondaryErrorCode int
@@ -35,61 +35,61 @@ func (c *SecondaryErrorCode) String() string {
 
 const (
 	// AdminUserValidation
-	AdminUserValidation_NameIsTooShort         SecondaryErrorCode = 0101
-	AdminUserValidation_NameIsTooLong          SecondaryErrorCode = 0102
-	AdminUserValidation_NameHasUnavailableChar SecondaryErrorCode = 0103
+	AdminUserValidation_NameIsTooShort         SecondaryErrorCode = 101
+	AdminUserValidation_NameIsTooLong          SecondaryErrorCode = 102
+	AdminUserValidation_NameHasUnavailableChar SecondaryErrorCode = 103
 
-	AdminUserValidation_ScreenNameIsTooShort         SecondaryErrorCode = 0201
-	AdminUserValidation_ScreenNameIsTooLong          SecondaryErrorCode = 0202
-	AdminUserValidation_ScreenNameHasUnavailableChar SecondaryErrorCode = 0203
+	AdminUserValidation_ScreenNameIsTooShort         SecondaryErrorCode = 201
+	AdminUserValidation_ScreenNameIsTooLong          SecondaryErrorCode = 202
+	AdminUserValidation_ScreenNameHasUnavailableChar SecondaryErrorCode = 203
 
-	AdminUserValidation_PasswordIsTooShort         SecondaryErrorCode = 0301
-	AdminUserValidation_PasswordIsTooLong          SecondaryErrorCode = 0302
-	AdminUserValidation_PasswordHasUnavailableChar SecondaryErrorCode = 0303
+	AdminUserValidation_PasswordIsTooShort         SecondaryErrorCode = 301
+	AdminUserValidation_PasswordIsTooLong          SecondaryErrorCode = 302
+	AdminUserValidation_PasswordHasUnavailableChar SecondaryErrorCode = 303
 
 	// LinkValidation
-	LinkValidation_URLIsIncipit SecondaryErrorCode = 0101
-	LinkValidation_URLIsInvalid SecondaryErrorCode = 0102
+	LinkValidation_URLIsIncipit SecondaryErrorCode = 101
+	LinkValidation_URLIsInvalid SecondaryErrorCode = 102
 
 	// AdminModel
-	AdminModel_FailedAdd            SecondaryErrorCode = 0101
-	AdminModel_FailedFind           SecondaryErrorCode = 0201
-	AdminModel_FindingUserNotFound  SecondaryErrorCode = 0202
-	AdminModel_FailedUpdate         SecondaryErrorCode = 0301
-	AdminModel_UpdatingUserNotFound SecondaryErrorCode = 0302
-	AdminModel_FailedDelete         SecondaryErrorCode = 0401
+	AdminModel_FailedAdd            SecondaryErrorCode = 101
+	AdminModel_FailedFind           SecondaryErrorCode = 201
+	AdminModel_FindingUserNotFound  SecondaryErrorCode = 202
+	AdminModel_FailedUpdate         SecondaryErrorCode = 301
+	AdminModel_UpdatingUserNotFound SecondaryErrorCode = 302
+	AdminModel_FailedDelete         SecondaryErrorCode = 401
 
 	// AdminAuthModel
-	AdminAuthModel_FailedToFindUser   SecondaryErrorCode = 0101
-	AdminAuthModel_UnmatchPassword    SecondaryErrorCode = 0102
-	AdminAuthModel_ExpiredToken       SecondaryErrorCode = 0103
-	AdminAuthModel_FailedToStoreToken SecondaryErrorCode = 0104
+	AdminAuthModel_FailedToFindUser   SecondaryErrorCode = 101
+	AdminAuthModel_UnmatchPassword    SecondaryErrorCode = 102
+	AdminAuthModel_ExpiredToken       SecondaryErrorCode = 103
+	AdminAuthModel_FailedToStoreToken SecondaryErrorCode = 104
 
 	// HashModel
-	HashModel_FailedGenerate SecondaryErrorCode = 0101
+	HashModel_FailedGenerate SecondaryErrorCode = 101
 
-	HashModel_FailedCompare SecondaryErrorCode = 0201
+	HashModel_FailedCompare SecondaryErrorCode = 201
 
 	// InstallerModel
 	// None
 
 	// LinkModel
-	LinkModel_FailedAdd            SecondaryErrorCode = 0101
-	LinkModel_FailedFind           SecondaryErrorCode = 0201
-	LinkModel_FindingLinkNotFound  SecondaryErrorCode = 0202
-	LinkModel_FailedUpdate         SecondaryErrorCode = 0301
-	LinkModel_UpdatingLinkNotFound SecondaryErrorCode = 0302
-	LinkModel_FailedDelete         SecondaryErrorCode = 0401
+	LinkModel_FailedAdd            SecondaryErrorCode = 101
+	LinkModel_FailedFind           SecondaryErrorCode = 201
+	LinkModel_FindingLinkNotFound  SecondaryErrorCode = 202
+	LinkModel_FailedUpdate         SecondaryErrorCode = 301
+	LinkModel_UpdatingLinkNotFound SecondaryErrorCode = 302
+	LinkModel_FailedDelete         SecondaryErrorCode = 401
 
 	// AdminUserHandler
-	AdminUserHandler_FailedBindJson SecondaryErrorCode = 0101
+	AdminUserHandler_FailedBindJson SecondaryErrorCode = 101
 
 	// AdminAuthHandler
-	AdminAuthHandler_FailedBindJson SecondaryErrorCode = 0101
+	AdminAuthHandler_FailedBindJson SecondaryErrorCode = 101
 
 	// LinkHandler
-	LinkHandler_FailedBindQuery SecondaryErrorCode = 0101
+	LinkHandler_FailedBindQuery SecondaryErrorCode = 101
 
 	// InstallHandler
-	InstallHandler_FailedBindJson SecondaryErrorCode = 0101
+	InstallHandler_FailedBindJson SecondaryErrorCode = 101
 )
