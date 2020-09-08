7c5436c1ffbf58a56fbaff0889b22009b75b6e81
usagiga <usagigawara@gmail.com>
2020-09-08 19:21:18 +0900

    Change admin's name as unique

--- back/entity/admin_user.go
+++ back/entity/admin_user.go
@@ -5,7 +5,7 @@ import "github.com/jinzhu/gorm"
 // AdminUser represents Incipit administrator
 type AdminUser struct {
 	gorm.Model
-	Name string
+	Name string `gorm:"unique"`
 	ScreenName string
 	Password string
 }
